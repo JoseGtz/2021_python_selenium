49a3dd73cd5708993c913aeb250827134d32baa0
Jose Gutierrez <jgutierrez@anexinet.com>
2021-02-18 18:40:28 -0600

    Module 02 updates

--- Module_02/youtube.py
+++ Module_02/youtube.py
@@ -4,20 +4,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 driver = get_driver('chrome')
-wait = WebDriverWait(driver, 5)
+wait = WebDriverWait(driver, 20)
 driver.get('https://www.youtube.com/')
 
 locator = (By.ID, 'search')
-search_textbox = wait.until(EC.visibility_of_element_located(locator))
+search_textbox = wait.until(EC.element_to_be_clickable(locator))
 search_textbox.send_keys('Selenium')
 
 locator = (By.ID, 'search-icon-legacy')
-search_button = wait.until(EC.visibility_of_element_located(locator))
+search_button = wait.until(EC.element_to_be_clickable(locator))
 search_button.click()
 
 locator = (By.ID, 'video-title')
 results = wait.until(EC.visibility_of_all_elements_located(locator))
-wait = WebDriverWait(driver, 15)
 print(f'Titulos: {len(results)}')
 for result in results:
     print(result.text)
