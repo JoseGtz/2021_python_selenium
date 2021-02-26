99a2e2633d26e1e0336555a9538d0b2c34683382
Jose Luis Gutierrez Valdez <josegutierrez@Joses-MacBook-Pro.local>
2021-02-26 12:56:11 -0600

    Updates

--- Module_03/tiktok.py
+++ Module_03/tiktok.py
@@ -1,5 +1,6 @@
+"""Search in tiktok"""
+from typing import List
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.common.keys import Keys
 from common.webdriver_factory import get_driver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,26 +8,35 @@ from selenium.webdriver.common.by import By
 
 
 def search(wait: WebDriverWait, value: str):
-    textbox_locator = By.ID, 'search'
-    textbox_element = wait.until(EC.element_to_be_clickable(textbox_locator))
-    textbox_element.clear()
-    textbox_element.send_keys(value)
-    textbox_element.send_keys(Keys.ENTER)
+    """Search in tiktok
+   :param wait: Web driver wait instance
+   :param value: Value to search
+   """
+    input_locator = (By.NAME, 'q')
+    textbox = wait.until(EC.element_to_be_clickable(input_locator))
+    textbox.clear()
+    textbox.send_keys(value)
+    btn_locator = (By.XPATH, "//button[@type='submit']")
+    btn = wait.until(EC.element_to_be_clickable(btn_locator))
+    btn.click()
 
-def get_catalog_info(wait: WebDriverWait) -> list:
-    result = []
-    result_locator = (By.CLASS_NAME, 'result_item')
-    search_results = wait.until(EC.visibility_of_all_elements_located(result_locator))
-    for search_result in search_results:
-        search_result: WebElement
-        name = search_result.find_element_by_class_name('title')
-        result.append(f'Name: {name.text}')
-    return result
+
+def print_results(wait: WebDriverWait):
+    """Print search results information.
+   :param wait:  Web driver wait instance
+   """
+    result: list[str] = []
+    profiles_loc = (By.XPATH, "//a[contains(@class, 'result-item')]")
+    profiles = wait.until(EC.visibility_of_all_elements_located(profiles_loc))
+    print(f'Results: {len(profiles)}')
+    for profile in profiles:
+        print(f'Tag {profile.text}')
+
 
 if __name__ == '__main__':
-    driver = get_driver('chrome')
-    wait = WebDriverWait(driver, 10)
-    driver.get('https://www.tiktok.com/?lang=es')
-    search(wait, 'Python')
-    print(get_catalog_info(wait))
-    driver.quit()
+    my_driver = get_driver('chrome')
+    my_wait = WebDriverWait(my_driver, 3)
+    my_driver.get('https://www.tiktok.com/?lang=es')
+    search(my_wait, 'Python')
+    print_results(my_wait)
+    my_driver.quit()
