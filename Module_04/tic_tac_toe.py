2d9022354993bb7a1d0f21f38f80bc7c6a86277a
Jose Luis Gutierrez Valdez <josegutierrez@Joses-MacBook-Pro.local>
2021-03-01 20:01:12 -0600

    Module 04

---
+++ Module_04/tic_tac_toe.py
@@ -0,0 +1,42 @@
+"""Automatically Tic Tac Toe"""
+import random
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.remote.webelement import WebElement
+from common.webdriver_factory import get_driver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+def winner(wait):
+    try:
+        locator = (By.CSS_SELECTOR, '.restart')
+        wait.until(EC.presence_of_element_located(locator))
+        return True
+    except TimeoutException:
+        return False
+
+
+def select_empty_box(wait):
+    locator = (By.CSS_SELECTOR, '.square')
+    elements = wait.until(EC.visibility_of_all_elements_located(locator))
+    empty = []
+    for box in elements:
+        box: WebElement
+        cell = box.find_element_by_tag_name('div')
+
+
+def print_game_stats(wait):
+    pass
+
+
+if __name__ == '__main__':
+    my_driver = get_driver('chrome')
+    my_wait = WebDriverWait(my_driver, 3)
+    my_driver.get('https://playtictactoe.org/')
+
+    while not winner(my_wait):
+        select_empty_box(my_wait)
+    print_game_stats(my_wait)
+
+    my_driver.quit()
